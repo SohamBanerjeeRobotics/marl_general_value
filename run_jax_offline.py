ef9cc02a518dc255bbb0a3f22757293aa3ff9444
Steven Morad <sm2558@cam.ac.uk>
2024-04-26 13:53:16 +0000

    wandb and standardize config

--- run_jax_offline.py
+++ run_jax_offline.py
@@ -5,51 +5,77 @@ import equinox as eqx
 import optax
 import h5py
 import tqdm
+import wandb
 
 from modules import GeneralQNetwork, greedy_policy
 from losses import update_general_qnet
 from tasks import add_rewards_to_dataset, make_global_navigation_tasks
 
-seed = 0
-batch_size = 1
-tau = jnp.array(1 / 200)
-num_agents = 1
-num_envs = 128
-key = jax.random.PRNGKey(seed)
-#env = vmas.make_env("sampling", num_envs=num_envs)
-epochs = 10
-gamma = jnp.array([0.99])
-eval_episodes = 10
 
 
 # opt setup
-lr_schedule = optax.constant_schedule(0.0001)
+config = {
+    "seed": 0,
+    "lr": 0.0001,
+    "gamma": jnp.array([0.99]),
+    "batch_size": 1,
+    "tau": jnp.array([1/200]),
+    "num_envs": 128,
+    "epochs": 20,
+    "eval_episodes": 10,
+    "q_config": {
+        "mlp_size": 256,
+        "head_size": 256,
+        "ensemble_size": 1,
+        "dropout": 0.1,
+    },
+    "task_size": 768,
+    "obs_size": 5,
+    "act_size": 9,
+    "simulator_weights": "data/dynamics_model_weights.eqx",
+}
+wandb.init('morlmarl', config=config)
+
+key = jax.random.PRNGKey(config["seed"])
+
+lr_schedule = optax.constant_schedule(config["lr"])
 opt = optax.chain(
     #optax.clip_by_global_norm(config["train"]["gradient_scale"]),
     optax.adamw(lr_schedule),
 )
-q_config = {
-    "mlp_size": 256,
-    "head_size": 256,
-    "ensemble_size": 1,
-    "dropout": 0.1,
-}
 
-q_function = GeneralQNetwork(obs_size=5, task_size=768, act_size=9, config=q_config, key=key)
-q_target = GeneralQNetwork(obs_size=5, task_size=768, act_size=9, config=q_config, key=key)
+q_function = GeneralQNetwork(
+    obs_size=config["obs_size"], 
+    task_size=config["task_size"], 
+    act_size=config["act_size"], 
+    config=config["q_config"], 
+    key=key
+)
+q_target = GeneralQNetwork(
+    obs_size=config["obs_size"], 
+    task_size=config["task_size"], 
+    act_size=config["act_size"], 
+    config=config["q_config"], 
+    key=key
+)
 opt_state = opt.init(eqx.filter(q_function, eqx.is_inexact_array))
 
 dataset_with_str = h5py.File("dataset.h5", "r")
 dataset = {k: v for k,v in dataset_with_str.items() if k != 'task_string'} 
 data_size = dataset['next_reward'].shape[0]
 
-simulator = StateTransitionModel(state_size=5, num_actions=9, dropout=0, key=jax.random.PRNGKey(0))
-simulator = eqx.tree_deserialise_leaves("data/dynamics_model_weights.eqx", simulator)
-eval_tasks = make_global_navigation_tasks(eval_episodes)
+simulator = StateTransitionModel(
+    state_size=config["obs_size"], 
+    num_actions=config["act_size"], 
+    dropout=0, 
+    key=jax.random.PRNGKey(0)
+)
+simulator = eqx.tree_deserialise_leaves(config["simulator_weights"], simulator)
+eval_tasks = make_global_navigation_tasks(config["eval_episodes"])
 
 # B, num_goals, S
 test_data = {k: v[:1] for k, v in dataset.items()}
-update_general_qnet(q_function, q_target, test_data, opt, opt_state, gamma, tau, key)
+update_general_qnet(q_function, q_target, test_data, opt, opt_state, config["gamma"], config["tau"], key)
 
 
 # TODO: Utilize negative reward for leaving boundaries
@@ -59,20 +85,20 @@ update_general_qnet(q_function, q_target, test_data, opt, opt_state, gamma, tau,
 # metrics
 td_error = jnp.array([jnp.inf])
 
-num_batches = (data_size + batch_size - 1) // batch_size
-for epoch in range(epochs):
+num_batches = (data_size + config["batch_size"] - 1) // config["batch_size"]
+for epoch in range(config["epochs"]):
     pbar = tqdm.tqdm(total=num_batches)
     for i in range(num_batches):
-        start_idx = i * batch_size
-        end_idx = min((i + 1) * batch_size, data_size)
+        start_idx = i * config["batch_size"]
+        end_idx = min((i + 1) * config["batch_size"], data_size)
         data_batch = {
             k: v[0:1] if k == "task_embedding" else v[start_idx:end_idx] 
             for k, v in dataset.items()
         }
 
         key, _ = jax.random.split(key)
-        q_function, q_target, td_error, qvalue, qtarget_value = eqx.filter_jit(update_general_qnet)(q_function, q_target, data_batch, opt, opt_state, gamma, tau, key)
-        out_str = f"Epoch {epoch}/{epochs} ql: {td_error.mean():0.4f} qv: {qvalue.mean():0.4f} qtv: {qtarget_value.mean():0.4f}"
+        q_function, q_target, td_error, qvalue, qtarget_value = eqx.filter_jit(update_general_qnet)(q_function, q_target, data_batch, opt, opt_state, config["gamma"], config["tau"], key)
+        out_str = f"Epoch {epoch}/{config['epochs']} ql: {td_error.mean():0.4f} qv: {qvalue.mean():0.4f} qtv: {qtarget_value.mean():0.4f}"
         pbar.set_description(out_str)
         pbar.update()
 
@@ -80,7 +106,7 @@ for epoch in range(epochs):
     ep_rewards = 0
     eval_q_function = eqx.nn.inference_mode(q_function)
     final_dists = []
-    for i in range(eval_episodes):
+    for i in range(config["eval_episodes"]):
         agent_state = jnp.array([0.0, 0.0, 0, 0, 0])
         done = False
         eval_task = {
@@ -107,8 +133,16 @@ for epoch in range(epochs):
             ep_reward += reward
             num_steps += 1
         ep_rewards += ep_reward
-        final_dists.append(jnp.linalg.norm(agent_state[:2] - eval_tasks["reward_kwargs"]["goal"][i]))
-    print(f"Episode reward: {ep_rewards.item() / eval_episodes:.2f}, final dist {final_dists}")
+        final_dists.append(jnp.linalg.norm(agent_state[:2] - eval_tasks["reward_kwargs"]["goal"][i]).item())
+    wandb.log({
+        "eval/mean_return": ep_rewards.item() / config['eval_episodes'],
+        "eval/mean_dist2goal": jnp.mean(jnp.array(final_dists)),
+        "train/loss": td_error.mean(),
+        "train/epoch": epoch,
+        "train/q_value_mean": qvalue.mean(),
+        "train/q_target_value_mean": qtarget_value.mean()
+    })
+    print(f"Episode reward: {ep_rewards.item() / config['eval_episodes']:.2f}, final dist {jnp.mean(jnp.array(final_dists))}")
 
 
 
