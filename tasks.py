bace7f94871973d2aac3c10f3465b8b8b1f6a2af
Steven Morad <sm2558@cam.ac.uk>
2024-04-27 15:24:21 +0100

    tasks change epsilon

--- tasks.py
+++ tasks.py
@@ -39,7 +39,7 @@ def make_navigation_goals_and_embeddings(num_tasks=1_000):
     task_strings = []
     task_embeddings = []
     goals = []
-    eps = 0.05
+    eps = 0.3
     for i in range(num_tasks):
         x = random.uniform(ARENA_BOUNDS_E[0] + eps, ARENA_BOUNDS_E[1] - eps)
         y = random.uniform(ARENA_BOUNDS_N[0] + eps, ARENA_BOUNDS_N[1] - eps)
