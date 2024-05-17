a5c95440079d91658cc65cc1e30c3b2b1349ee58
S. Morad <sm2558@cam.ac.uk>
2024-05-17 11:40:38 +0100

    recording software works

--- robomaster_control.py
+++ robomaster_control.py
@@ -6,98 +6,38 @@ from jax import random
 import pickle
 from typing import Dict, Any
 from modules import GeneralMAQNetwork, GeneralQNetwork, greedy_policy
+import yaml
 
 
 
 # Load embeddings and corresponding natural language commands
-mappings = pickle.loads(open('robomaster_control.pkl', "rb").read())
+train_mappings = pickle.loads(open('robomaster_control_train.pkl', "rb").read())
+eval_mappings = pickle.loads(open('robomaster_control_eval.pkl', "rb").read())
 # Convert action index to command
 
 from constants import ACTION_MAPPING
 
-config = {
-    "seed": 0,
-    "lr": 0.0001,
-    "loss": "meanq",
-    "weight_decay": 0.0001,
-    "gamma": jnp.array([0.95]),
-    "batch_size": 32,
-    "tau": jnp.array([1/1000]),
-    "epochs": 3000,
-    "eval_interval": 50,
-    "q_config": {
-        "mlp_size": 384,
-        "head_size": 384,
-        "ensemble_size": 1,
-        "dropout": 0.0,
-        "ensemble_size": 2,
-        "ensemble_reduce": "min",
-    },
-    "task_size": 768,
-    "obs_size": 4,
-    "act_size": 9,
-    "simulator_weights": "data/dynamics_model_weights.eqx",
-}
+cfg_path = "experiments/real_soft-2.0.yaml"
+with open(cfg_path) as f:
+    config = yaml.safe_load(f)
+
+config['seed'] = 0
+key = jax.random.PRNGKey(0)
 
 # Model config and setup
-q_function = GeneralQNetwork(
+ma_q_function = GeneralMAQNetwork(
     obs_size=config["obs_size"], 
     task_size=config["task_size"], 
     act_size=config["act_size"], 
     config=config["q_config"], 
-    key=jax.random.PRNGKey(0)
+    key=key,
 )
-q_function = eqx.tree_deserialise_leaves(f"models/ne-0-950-1.94.eqx", q_function)
+model_weights = 'models/soft-2.0-126000.eqx'
+ma_q_function = eqx.tree_deserialise_leaves(model_weights, ma_q_function)
 
 
-# To map states and tasks to velocities, use policy wrapper
-# e.g., policy_wrapper(state_numpy_vec, mappings['Agent 0, navigate to the west edge'])
-# where state_numpy_vec is of shape 5: (e, n, ve, vn, yaw_angle)
-# policy_wrapper(jnp.array([0, 0, 0, 0, 0]), mappings['Agent 0, navigate to the west edge'])
-def policy_wrapper(q_function, state, task_embedding):
-    action_idx = eqx.filter_jit(greedy_policy)(q_function, state, task_embedding, key=jax.random.PRNGKey(0))
-    return action_idx
-
-
-## Multiagent
-
-num_agents = 3
-ma_config = {
-    "seed": 0,
-    "lr": 0.0001,
-    "loss": "maxq",
-    "weight_decay": 0.0001,
-    "warmup_epochs": 100,
-    "gamma": jnp.array([0.95]),
-    "batch_size": 256,
-    "num_agents": num_agents,
-    "tau": jnp.array([1/2000]),
-    "epochs": 100_000,
-    "eval_interval": 1000,
-    "eval_trials": 3,
-    "q_config": {
-        "mlp_size": 1024,
-        "head_size": 1024,
-        "dropout": 0.0,
-        "ensemble_size": 2,
-        "ensemble_reduce": "min",
-    },
-    "task_size": 768,
-    "obs_size": 4,
-    "act_size": 9,
-    "simulator_weights": "data/dynamics_model_weights.eqx",
-}
-
-# Model config and setup
-ma_q_function = GeneralMAQNetwork(
-    obs_size=ma_config["obs_size"], 
-    task_size=ma_config["task_size"], 
-    act_size=ma_config["act_size"], 
-    config=ma_config["q_config"], 
-    key=jax.random.PRNGKey(0)
-)
-ma_q_function = eqx.tree_deserialise_leaves(f"models/ne-nagents-3-seed-0-epoch-99000-return-0.79.eqx", ma_q_function)
-
-def ma_policy_wrapper(ma_q_function, state, task_embedding):
+def ma_policy_wrapper(ma_q_function, state, task_embedding, key):
+    key, sample_key = jax.random.split(key)
     q_value = ma_q_function(state, task_embedding, jax.random.PRNGKey(0))
-    return q_value.argmax(axis=-1)
+    return jax.random.categorical(sample_key, q_value * 100.0), key
+    #return q_value.argmax(axis=-1)
--- robomaster_control_nodes.py
+++ robomaster_control_nodes.py
@@ -1,6 +1,7 @@
 from constants import ACTION_MAPPING, ARENA_BOUNDS_N, ARENA_BOUNDS_E, ACTION_IDX, ROBOT_DIAMETER
 import rclpy
 from rclpy.node import Node
+from std_msgs.msg import String, Float32
 
 from freyja_msgs.msg import ReferenceState, CurrentState
 
@@ -12,6 +13,10 @@ import math, random
 import numpy as np
 import pandas as pd
 import time
+
+random.seed(0)
+np.random.seed(0)
+mappings = RControl.eval_mappings # Can be train or eval
 
 
 class Robot:
@@ -38,20 +43,27 @@ class RoboMasterBase(Node):
     states = []
     actions = []
     action_vels = []
+    ros_timestamps = []
 
     def setup(self):
-        self.max_time = 900;  # seconds, expt duration
+        self.max_time = 60 * 10;  # seconds, expt duration
         self.robot_names = ["robomaster_1", "robomaster_2", "robomaster_3"]
         self.n_robots = len(self.robot_names)
 
         self.robots = [None]*self.n_robots
         self.state_subs = [None]*self.n_robots
         self.ref_pubs = [None]*self.n_robots
+        self.goal_pubs = [None]*self.n_robots
+        self.string_pubs = [None]*self.n_robots
+        self.dist2goal_pubs = [None]*self.n_robots
         for idx in range(self.n_robots):
             name = self.robot_names[idx]
             self.robots[idx] = Robot(name)
             self.state_subs[idx] = self.create_subscription(CurrentState, name+"/current_state", self.robots[idx].state_callback, 1)
             self.ref_pubs[idx] = self.create_publisher(ReferenceState, name+"/reference_state", 1)
+            self.goal_pubs[idx] = self.create_publisher(ReferenceState, name+"/goal_state", 1)
+            self.dist2goal_pubs[idx] = self.create_publisher(Float32, name+"/dist_to_goal", 1)
+            self.string_pubs[idx] = self.create_publisher(String, name+"/goal_string", 1)
             print("Initialised: ", name)
 
         self.print_help_once = True;
@@ -77,7 +89,6 @@ class RoboMasterBase(Node):
         action_idx[collision.astype(bool)] = ACTION_IDX["0"]
         if np.any(collision):
             print("Collision detected: ", collision)
-            breakpoint()
         return action_idx
 
 
@@ -85,9 +96,6 @@ class RoboMasterBase(Node):
         for r in range(len(self.robots)):
             if not self.robots[r].ready:
                 print(f"Waiting for state for robot {self.robot_names[r]}..")
-                if self.print_help_once:
-                    print("Av. keys: ", RControl.mappings.keys())
-                    self.print_help_once = False
                 return
 
         states = [
@@ -98,7 +106,7 @@ class RoboMasterBase(Node):
         self.states.append(states)
 
         action_idx = self.get_action_idx(states)
-        action_idx = self.get_safe_action(states, action_idx)
+        #action_idx = self.get_safe_action(states, action_idx)
         action_vel = np.stack([ACTION_MAPPING[idx.item()] for idx in action_idx], axis=0)
         self.actions.append(action_idx)
         self.action_vels.append(action_vel)
@@ -109,13 +117,15 @@ class RoboMasterBase(Node):
             #f"action: {ACTION_MAPPING[action_idx]}"
             f"action: {action_str}\n"
             #f"state (pn/pe/ve/vn): {state[0]:.2f}/{state[1]:.2f}/{state[2]:.2f}/{state[3]:.2f}" 
-            f"state (pn/pe/ve/vn): {states}"
+            #f"state (pn/pe/ve/vn): {states}"
         )
 
+        self.ros_timestamps.append(self.get_clock().now().nanoseconds)
         for r in range(len(self.robots)):
             rstate = ReferenceState()
             rstate.vn = action_vel[r, 0].item()
             rstate.ve = action_vel[r, 1].item()
+            rstate.header.stamp = self.get_clock().now().to_msg()
             self.ref_pubs[r].publish(rstate)
 
         # book-keeping
@@ -171,35 +181,65 @@ class RoboMasterCollect(RoboMasterBase):
 
 
 class RoboMasterEval(RoboMasterBase):
+    goals = []
+    key = RControl.key
+
     def __init__(self):
         super().__init__("robomaster_eval")
         self.setup()
-        self.task_idx = list(range(len(self.robots)))
+        self.task_idx = np.random.choice(len(mappings), len(self.robots), replace=False)
+        self.task_string_idx = [0] * len(self.robots)
+        self.task_string_idx = np.random.randint(
+            [len(mappings[idx]) for idx in self.task_idx]
+        )
         self.task_timer = 0
-        self.max_task_time = 15
+        self.max_task_time = 30
 
     def get_action_idx(self, state):
-        #prompt_str = list(RControl.mappings.keys())[self.task_idx]
-        prompt_strs = [
-            list(RControl.mappings.keys())[idx]
-            for idx in self.task_idx
-        ]
-        #embedding, goal = RControl.mappings[prompt_str]['embedding'], RControl.mappings[prompt_str]['goal']
-        embeddings = np.stack([
-            RControl.mappings[prompt_str]['embedding']
-            for prompt_str in prompt_strs 
-        ], axis=0)
+        tasks = [mappings[idx] for idx in self.task_idx]
+        # First index is the robot idx, second is the task idx
+        prompt_strs = [tasks[i][idx]['task_string'] for i, idx in enumerate(self.task_string_idx)]
+        embeddings = np.stack(
+            [tasks[i][idx]['embedding'] for i, idx in enumerate(self.task_string_idx)], axis=0
+        )
+        goals = np.stack(
+            [tasks[i][idx]['goal'] for i, idx in enumerate(self.task_string_idx)], axis=0
+        )
 
-        action_idx = RControl.ma_policy_wrapper(
+        action_idx, self.key = RControl.ma_policy_wrapper(
             RControl.ma_q_function,
             state, 
             embeddings,
+            self.key
         )
+
+        # Log goals for posterity
+        self.goals.append(goals)
+        goal_dists = np.linalg.norm(goals - state[:, :2], axis=-1)
+        for i in range(self.n_robots):
+            goal_msg = ReferenceState()
+            goal_msg.pn, goal_msg.pe = goals[i, 0], goals[i, 1]
+            goal_msg.header.stamp = self.get_clock().now().to_msg()
+            self.goal_pubs[i].publish(goal_msg)
+
+            str_msg = String()
+            str_msg.data = prompt_strs[i]
+            self.string_pubs[i].publish(str_msg)
+
+            dist = Float32()
+            dist.data = goal_dists[i]
+            self.dist2goal_pubs[i].publish(dist)
+
+        print(f"Mean dist. {np.mean(goal_dists)}")
+
         self.task_timer += 1
         if self.task_timer >= self.max_task_time:
             print("Next task!")
-            #self.task_idx = [(idx + 1) % len(RControl.mappings) for idx in self.task_idx]
-            self.task_idx = np.random.choice(len(RControl.mappings), len(self.robots), replace=False)
+            self.task_idx = np.random.choice(len(mappings), len(self.robots), replace=False)
+            tasks = [mappings[idx] for idx in self.task_idx]
+            self.task_string_idx = np.random.randint(
+                [len(tasks[i]) for i in range(self.n_robots)]
+            )
             self.task_timer = 0
         print(
                 f"Task: {prompt_strs} ({self.task_timer:.0f}/{self.max_task_time:.0f}s)"
@@ -207,30 +247,44 @@ class RoboMasterEval(RoboMasterBase):
         return action_idx
 
     def teardown(self):
+        dfs = []
         state = np.stack(self.states[:-1])
         next_state = np.stack(self.states[1:])
         action = np.stack(self.actions[:-1])
         action_vels = np.stack(self.action_vels[:-1])
-        state = pd.DataFrame.from_dict({
-            "state.pn": state[:,0],
-            "state.pe": state[:,1],
-            "state.vn": state[:,2],
-            "state.ve": state[:,3],
-        })
-        next_state = pd.DataFrame.from_dict({
-            "next_state.pn": next_state[:,0],
-            "next_state.pe": next_state[:,1],
-            "next_state.vn": next_state[:,2],
-            "next_state.ve": next_state[:,3],
-        })
-        action_vels = pd.DataFrame.from_dict({
-            "action_vel.vn": action_vels[:,0],
-            "action_vel.ve": action_vels[:,1],
-        })
-        df = pd.DataFrame({
-            **state,
-            **next_state,
-            **action_vels,
-            "action": action,
-        })
+        goals = np.stack(self.goals[:-1])
+        for idx, name in enumerate(self.robot_names):
+            s = pd.DataFrame.from_dict({
+                "state.pn": state[:, idx, 0],
+                "state.pe": state[:, idx, 1],
+                "state.vn": state[:, idx, 2],
+                "state.ve": state[:, idx, 3],
+            })
+            next_s = pd.DataFrame.from_dict({
+                "next_state.pn": next_state[:, idx, 0],
+                "next_state.pe": next_state[:, idx, 1],
+                "next_state.vn": next_state[:, idx, 2],
+                "next_state.ve": next_state[:, idx, 3],
+            })
+            v = pd.DataFrame.from_dict({
+                "action_vel.vn": action_vels[:, idx, 0],
+                "action_vel.ve": action_vels[:, idx, 1],
+            })
+            g = pd.DataFrame.from_dict({
+                "goals.pn": action_vels[:, idx, 0],
+                "goals.pe": action_vels[:, idx, 1],
+            })
+            df = pd.DataFrame({
+                **s,
+                **next_s,
+                **v,
+                **g,
+                "action": action[:, idx],
+                "ros_timestamp_ns": self.ros_timestamps[:-1],
+            })
+            df['robot_name'] = name
+            df['robot_idx'] = idx
+            dfs.append(df)
+        df = pd.concat(dfs).reset_index(drop=True)
+        print('Mean dist to goal', np.linalg.norm(state[:, :, :2] - goals, axis=-1).mean())
         df.to_csv(f"data/robomaster_eval_{int(time.time())}.csv")
